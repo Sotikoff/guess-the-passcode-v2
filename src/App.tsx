94586527f05c696b1c01b1157841ec375252df9a
valera <iamsotikoff@gmail.com>
2025-09-14 13:48:18 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Stub } from './components/Stub'
+
+function App() {
+
+  return (
+    <>
+      <Stub />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Stub.tsx
@@ -0,0 +1,7 @@
+export const Stub = () => {
+    return (
+        <h1>
+            STUB
+        </h1>
+    )
+}
